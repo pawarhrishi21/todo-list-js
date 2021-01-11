907eca37221265251361ad1a5e5132d60c0cab05
Hrishikesh <pawarhrishi21@gmail.com>
2021-01-11 21:49:42 +0530

    database integrated through mongoose

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
-var itemList = [];
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post("/",function(req,res){
-    var item = req.body.itemAdded;
-    console.log(item);
-    itemList.push(item);
-    res.redirect("/");
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+    name: String,
+}
+
+const Item = mongoose.model("Item", itemsSchema);
+
+
+const defaultItem1 = new Item({
+    name: "Welcome! Click + to add a task.",
 })
+
+const defaultItem2 = new Item({
+    name: "Select the checkbox to remove a task.",
+})
+
+const defaultItems = [defaultItem1, defaultItem2];
+
 
 
 app.get("/",function(req,res){
     var date = new Date();
-
     var today = date.toLocaleDateString("en-US", {
         "weekday": "long",
         "day": "numeric",
@@ -27,7 +39,43 @@ app.get("/",function(req,res){
         "month": "long"
     });
 
-    res.render("display", { dateDisplay : today, itemListDisplay : itemList  });
+    Item.find(function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            if(result.length===0){
+                res.render("display", {dateDisplay : "Today", itemListDisplay : defaultItems});
+            }
+            else{
+                res.render("display", {dateDisplay : "Today", itemListDisplay : result});
+            }
+        }
+    });
+    
+})
+
+app.post("/",function(req,res){
+    //Inserting the newItem into the Database
+    let newItem = new Item({
+        name: req.body.itemAdded,
+    })
+    
+    newItem.save()
+    res.redirect("/");
+})
+
+app.post("/delete",function(req,res){
+    //Delete the items from Database which get checked
+    console.log(req.body);
+    Item.findByIdAndDelete(req.body.check, function(err){
+        if(err){
+            console.log(error);
+        }else{
+            console.log("Succesfully deleted from DB");
+            res.redirect("/");
+        }
+    })
+
 
 })
 
