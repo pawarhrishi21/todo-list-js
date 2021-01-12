3643fb9166a81754000aaf5c408e97e821d05818
Hrishikesh <pawarhrishi21@gmail.com>
2021-01-12 15:37:20 +0530

    Database integration completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const _ = require("lodash");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useFindAndModify', false); // o/w Depreciation warning for using findOneAndUpdate() 
 
 const itemsSchema = {
     name: String,
@@ -28,57 +30,85 @@ const defaultItem2 = new Item({
 
 const defaultItems = [defaultItem1, defaultItem2];
 
+const customListSchema = mongoose.Schema({
+    customName: String,
+    customList: [itemsSchema],
+});
 
+const CustomList = mongoose.model("customList", customListSchema);
 
 app.get("/",function(req,res){
-    var date = new Date();
-    var today = date.toLocaleDateString("en-US", {
-        "weekday": "long",
-        "day": "numeric",
-        "year": "numeric",
-        "month": "long"
-    });
 
     Item.find(function(err,result){
         if(err){
             console.log(err);
         }else{
             if(result.length===0){
-                res.render("display", {dateDisplay : "Today", itemListDisplay : defaultItems});
+                res.render("display", {displayName : "Today", itemListDisplay : defaultItems});
             }
             else{
-                res.render("display", {dateDisplay : "Today", itemListDisplay : result});
+                res.render("display", {displayName : "Today", itemListDisplay : result});
             }
         }
     });
-    
 })
 
-app.get("/:param",function(req,res){
-    console.log(req.params.param);
+app.get("/:customListName",function(req,res){
+    const customListName = _.capitalize(req.params.customListName);
+    CustomList.findOne({customName: customListName}, function(err,foundList){
+        if(!err){
+            if(foundList === null){
+                const customList = new CustomList({
+                    customName: customListName,
+                    customList: defaultItems,
+                });
+                customList.save();
+                res.redirect("/" + customListName);
+            }else{
+                res.render("display",{displayName: customListName, itemListDisplay: foundList.customList});
+            }
+        }
+    })
 })
 
 app.post("/",function(req,res){
-    //Inserting the newItem into the Database
     let newItem = new Item({
         name: req.body.itemAdded,
     })
-    
-    newItem.save()
-    res.redirect("/");
+    if(req.body.list == "Today"){
+        //Inserting the newItem into the Database        
+        newItem.save()
+        res.redirect("/");
+    }else{
+        CustomList.findOne({customName:req.body.list}, function(err,foundList){
+            if(!err){
+                foundList.customList.push(newItem);
+                foundList.save();
+                res.redirect("/" + req.body.list);
+            }
+        })
+    }
 })
 
 app.post("/delete",function(req,res){
     //Delete the items from Database which get checked
-    console.log(req.body);
-    Item.findByIdAndDelete(req.body.check, function(err){
-        if(err){
-            console.log(error);
-        }else{
-            console.log("Succesfully deleted from DB");
-            res.redirect("/");
-        }
-    })
+    if(req.body.list == "Today"){
+        Item.findByIdAndDelete(req.body.check, function(err){
+            if(err){
+                console.log(error);
+            }else{
+                console.log("Succesfully deleted from DB");
+                res.redirect("/");
+            }
+        })
+    }else{
+        CustomList.findOneAndUpdate({customName: req.body.list}, {$pull:{customList: {_id: req.body.check}}}, function(err,foundList){
+            if(!err){
+                res.redirect("/" + req.body.list);
+            }
+        } )
+    }
+
 })
 
 app.listen(3000,function(){
